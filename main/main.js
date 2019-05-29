16b51ee16d0b30da3808e69fab3ea9e71328c38f
minglusu <18159351287@163.com>
2019-05-29 22:42:32 +0800

    led

--- main/main.js
+++ main/main.js
@@ -1,4 +1,33 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+"use strict";
+
+module.exports = function main(input) {
+	let numbers = `
+._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+|.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+|_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
+`;
+	let splitArr = numbers.split(/   |\n/),
+		numberArr = [],
+		k= 0;
+	for(let i=0, l=splitArr.length;i<l;i++){
+		let numberStr = splitArr[i];
+		if(''==numberStr) continue;
+		if(10==k) k=0;
+		if(!(k in numberArr)) numberArr[k] = [];
+		numberArr[k].push(numberStr);
+		k++;
+	}
+    console.log(numberArr);
+
+    let output = '';
+    for(let i=0; i<3; i++){
+    	for(let k = 0, l=input.length; k<l;k++){
+    		let number = input[k];
+    		output += numberArr[number][i];
+    		if(k!=l-1) output += " ";
+    	}
+    	output += "\n";
+    }
+    console.log(output);
+    return output;
 };
